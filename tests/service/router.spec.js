5443de8c749b85127185e4132eab201dbf875a76
沐风 <mufeng@mufengdeiMac.local>
2018-12-20 09:46:15 +0800

    1

--- tests/service/router.spec.js
+++ tests/service/router.spec.js
@@ -17,7 +17,7 @@ describe("node接口测试", function () {
             .expect(200)
             .end(function (err, res) {
                 if (err) return done(err);
-                if (res.body.data == "Hello Worlxxxxd!") {
+                if (res.body.data == "Hello Worlds!") {
                     done();
                 } else {
                     done(new Error("接口数据异常"));
